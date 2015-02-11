2922cc3c53115bed9032c6675510793d21f53348
jjossie <joeljossie@gmail.com>
2015-02-11 08:15:02 -0600

    created person.java

---
+++ Assignment3/src/Person.java
@@ -0,0 +1,12 @@
+
+public class Person {
+	private String fname;
+	private String lname;
+	public Person(String f, String l){
+		fname = f;
+		lname = l;
+	}
+	public String toString(){
+		return "" + lname + ", " + fname;
+	}
+}
