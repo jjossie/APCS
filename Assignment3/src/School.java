2d058157192cedb71618f22236290965be830477
jjossie <joeljossie@gmail.com>
2015-02-13 08:38:34 -0600

    I have no idea what just happened

--- Assignment3/src/School.java
+++ Assignment3/src/School.java
@@ -3,8 +3,31 @@ public class School {
 	ArrayList<Student> students = new ArrayList<Student>();
 	ArrayList<Teacher> teachers = new ArrayList<Teacher>();
 
+	public String getGradeLevel(int g){
+		String str = "";
+		for (Student s: students){
+			if (s.getLevel() == g){
+				str = str + s.toString() + "\n";
+			}
+		}
+		return str;
+	}
+
 	public School(ArrayList<Student> students, ArrayList<Teacher> teachers) {
 		this.students = students;
 		this.teachers = teachers;
+
+	}
+	public String toString(){
+		String str = "";
+		str = "Faculty: \n";
+		for (Teacher t: teachers){
+			str = str + t.toString() + '\n';
+		}
+		str = str + "\nStudent Body:\n";
+		for (Student s: students){
+			str = str + s.toString() + '\n';
+		}
+		return str;
 	}
 }
